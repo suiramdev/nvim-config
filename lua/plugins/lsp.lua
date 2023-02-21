b3a341c3c4900972251f36505df81e217c6e1a1b
suiramdev <mariusnouchet@gmail.com>
2023-02-21 21:27:46 +0100

    :sparkles: Ensure more lsp languages to be installed by default

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,18 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
+					"cssls",
+					"cssmodules_ls",
+					"unocss",
+					"eslint",
+					"graphql",
+					"html",
+					"jsonls",
+					"quick_lint_js",
+					"tsserver",
+					"vtsls",
+					"sqlls",
+					"tailwindcss",
 				},
 			});
 		end,
@@ -18,6 +30,17 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			require("lspconfig").lua_ls.setup({})
+			require("lspconfig").cssls.setup({})
+			require("lspconfig").cssmodules_ls.setup({})
+			require("lspconfig").unocss.setup({})
+			require("lspconfig").eslint.setup({})
+			require("lspconfig").graphql.setup({})
+			require("lspconfig").html.setup({})
+			require("lspconfig").jsonls.setup({})
+			require("lspconfig").quick_lint_js.setup({})
+			require("lspconfig").tsserver.setup({})
+			require("lspconfig").vtsls.setup({})
+			require("lspconfig").tailwindcss.setup({})
 		end,
 	},
 }
